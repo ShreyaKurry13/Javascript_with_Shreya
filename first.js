1de8aef11acc47d9e50fe4d860d7ed3dffd87325
ShreyaKurry13 <109572940+ShreyaKurry13@users.noreply.github.com>
2024-09-03 00:08:50 +0530

    js

---
+++ first.js
@@ -0,0 +1,35 @@
+//objects
+const student = {
+    name: 'John Doe',
+    age: 20,    
+    grades: [90, 85, 95],
+    address: "mumbai"
+};
+console.log(student);
+
+//arithmetic operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ",a ,"& b = ", b)
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);    
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a % b = ", a%b); //modulus --> remainder
+// console.log("a ** b = ", a**b); //exponentiation 5^2 =5*5=25
+
+//unary operators (where only one variable is needed)
+// a++ ,a--
+// let a = 5;
+// let b = 2;
+// console.log("a = ",a ,"& b = ", b)
+// // a--;
+// console.log("a++ = ",++a);
+
+//assignment operators
+let a = 5;
+let b = 2;
+a+= 5;
+console.log("a = ",a);
